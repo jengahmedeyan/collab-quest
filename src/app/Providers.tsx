651b1fb35ca0b0b7fabce06f4c0191ddbc8db1fd
jengahmedeyan <jengahmedeyan@gmail.com>
2024-10-25 06:55:14 +0000

    Compose providers

---
+++ src/app/Providers.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { ReactNode } from "react";
+import { ThemeProvider } from "@/app/themeProvider";
+import { Toaster } from "@/components/ui/toaster";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ConvexReactClient } from "convex/react";
+
+export default function Providers({ children }: { children: ReactNode }) {
+  const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Toaster />
+            {children}
+          </ThemeProvider>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+}
